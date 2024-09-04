07c691b3134841060d370612a71f25346beacee1
Radik <khamzin.radik.87@mail.ru>
2024-09-04 12:25:47 +0500

    '3task'

--- modele_12/task_3.py
+++ modele_12/task_3.py
@@ -1,33 +1,39 @@
 # -*- coding: cp1251 -*-
 
 
-def summ (a,b):
-    str_summ=str(a)+str(b)
+def summ (a):
+    str_summ=str(a)
     coumt =int(len(str_summ))
     summ=0
     for i in range (0,coumt,1):
         summ+=int(str_summ[i])
     return summ
 
-def maximum(a,b):
+def maximum(a):
     max=0
-    if a>b:
-        max=a
-    else:
-        max=b
+    a=str(a)
+    count = int(len(a))
+    
+    for i in range (0,count,1):
+       
+        if max<int(a[i]):
+           max=int(a[i])
+    
     return max
 
-def minimum (a,b):
-    min=0
-    if a<b:
-        min=a
-    else :
-        min=b
-
+def minimum (a):
+    
+    a=str(a)
+    count = int(len(a))
+    min=int(a[0])
+    for i in range (0,count,1):
+        if min>int(a[i]):
+           min=int(a[i])
     return min
+
 while (True):
     nambrt_1=int(input('введите 2 числа '))
-    namber_2=int(input())
+  
     
 
     otvet=int(input ('введите 1 усли вы хотите сложить цифры, 2 если хотите узнать наибольшее из 2х чисел, 3 если минимальную из 2х чисел или 0 для выхода'))
@@ -38,9 +44,9 @@ while (True):
         if otvet==0:
             False
     if otvet==1:
-        print (summ (nambrt_1,namber_2))
+        print (summ (nambrt_1))
     if otvet==2:
-        print (maximum(nambrt_1,namber_2))
+        print (maximum(nambrt_1))
     if otvet==3:
-        print (minimum(nambrt_1,namber_2))
+        print (minimum(nambrt_1))
 
